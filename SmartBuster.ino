7dc7c0609a9f13fc255e2395435c3c71815f7d5b
RefaelIvgi <109313481+RefaelIvgi@users.noreply.github.com>
2022-12-14 15:15:56 +0200

    Add files via upload

---
+++ SmartBuster.ino
@@ -0,0 +1,33 @@
+
+#include <msp430.h>
+
+void setup() {
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  int sensorValue= analogRead(A3);
+  Serial.println(sensorValue);
+  if(sensorValue > 100){
+    sendsms();
+  }
+
+}
+void sendsms(){
+
+Serial.println("AT\r");
+delay(1000);
+
+Serial.println("AT+CMGF = 1 \r");
+delay(1000);
+
+Serial.println("AT+CMGF = \"+972XXXXXX\"\r");
+delay(1000);
+
+Serial.println("Alert From SmartBuster");
+delay(1000);
+
+Serial.println((char)26);
+delay(1000);
+}
